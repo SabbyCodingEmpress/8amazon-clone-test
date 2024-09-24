3df70d1f28007de2b2fc2096552d5953f80168da
SabbyCodingEmpress <sabina_guerrero@yahoo.com>
2024-09-23 21:45:20 -0700

    888

---
+++ 8amazon-clone/next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'fakestoreapi.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'links.papareact.com',
+            },
+        ],
+    },
+};
